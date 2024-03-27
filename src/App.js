60013c5bdc14a77662d1776de6555a05cafe858a
Amruth L P <134767358+AmruthGowda91200@users.noreply.github.com>
2024-03-27 14:34:39 +0530

    sdd

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs.js";
 import Contact from "./Pages/Contact.js";
+import Reg from "./Pages/Reg.js";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/" element={<Reg />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </Router>
